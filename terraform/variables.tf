76d88669e1b3bacf19cecc72feffdf5e6a9e9542
root <root@DESKTOP-M3CKKGV.>
2025-03-12 23:09:06 -0500

    first circle

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_azs" {
+    type = list(string)
+    description = "A list of availability zones in the region"
+    default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "public_subnet_cidrs" {
+    type = list(string)
+    description = "A list of CIDR blocks for the public subnets"
+    default = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+}
+
